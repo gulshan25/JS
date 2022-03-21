f0404c15df661a8d55aa3ef02c1608f2e5ea5b89
gulshan25 <g.r.tanny@gmail.com>
2022-03-21 23:16:29 +0600

    first commit

---
+++ test.js
@@ -0,0 +1,31 @@
+// Create a function to print any numbers Fibonacci
+
+var n;
+var fi = [];
+
+fi[0] = 0;
+fi[1] = 1;
+for (n = 2; n <= 20; n++) {
+  fi[n] = fi[n - 1] + fi[n - 2];
+  console.log(fi[n]);
+}
+
+// Create a function to calculate even numbers
+
+const num = prompt("Enter a number: ");
+
+if (num % 2 == 0) {
+  console.log("The number is even.");
+} else {
+  console.log("The number is odd.");
+}
+
+// Create a function to calculate odd numbers
+
+const number = prompt("Enter a number: ");
+
+if (number % 1 != 0) {
+  console.log("The number is even.");
+} else {
+  console.log("The number is odd.");
+}
